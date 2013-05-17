4844a30ed3845360edab7e7eeb6a96cdcfd9656e
Brandon Hilkert <brandonhilkert@gmail.com>
2013-05-17 12:06:15 -0400

    Check out the json

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@ Bundler.require
 
 get '/' do
   'Got it!'
+  send_to_dev_underground 'Hey Players!'
+end
+
+post '/' do
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end
+
+def send_to_dev_underground(msg)
+  client = HipChat::Client.new(ENV['HIPCHAT_API'])
+  client['Dev Underground'].send('Mr. Hubot', msg)
 end
