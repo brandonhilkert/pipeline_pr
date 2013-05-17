ec958474fbf63a1d20983c273db07c2654a9df6f
Brandon Hilkert <brandonhilkert@gmail.com>
2013-05-17 17:54:05 -0400

    Don't log

--- app.rb
+++ app.rb
@@ -5,34 +5,12 @@ get '/' do
   'OMG'
 end
 
-def redis
-  @redis ||= (
-    url = URI(ENV['REDISTOGO_URL'] || "redis://127.0.0.1:6379")
-
-    base_settings = {
-      host: url.host,
-      port: url.port,
-      db: url.path[1..-1],
-      password: url.password
-    }
-
-    Redis::Namespace.new("pr:", redis: Redis.new(base_settings))
-  )
-end
-
 post '/' do
-  redis.set 'params', params
-
-  if params[:payload]
-    payload = JSON.parse(params[:payload])
-    redis.set 'github-payload', params[:payload]
-  end
-
   # Only trigger message when a new PR is opened
-  # if payload['action'] == 'opened'
-  #   hipchat_msg = format_text_for_pr_message(payload['pull_request'])
-  #   send_to_dev_underground(hipchat_msg)
-  # end
+  if payload['action'] == 'opened'
+    hipchat_msg = format_text_for_pr_message(payload['pull_request'])
+    send_to_dev_underground(hipchat_msg)
+  end
 end
 
 def format_text_for_pr_message(pr)
