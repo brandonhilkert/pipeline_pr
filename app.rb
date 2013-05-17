84a81f20ecc45aecaafc4179f2c172f7c6ea7edd
Brandon Hilkert <brandonhilkert@gmail.com>
2013-05-17 13:17:57 -0400

    Parse payload

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
 require 'bundler'
-require 'logger'
 Bundler.require
-
-$logger = Logger.new('app.log')
 
 get '/' do
   'Got it!'
@@ -10,10 +7,20 @@ get '/' do
 end
 
 post '/' do
-  $logger.info params[:payload].inspect
-  # puts params[:payload]
-  # push = JSON.parse(params[:payload])
-  # "I got some JSON: #{push.inspect}"
+  url = params[:payload]['head_commit']['url']
+  msg = params[:payload]['head_commit']['message']
+  user = params[:payload]['head_commit']['author']['username']
+  hipchat_msg = format_text_for_pr_message(url, user, msg)
+  send_to_dev_underground(hipchat_msg)
+end
+
+def format_text_for_pr_message(url, user, msg)
+  str = ""
+  str << "New Pull Request from #{user}"
+  str << "\n"
+  str << msg
+  str << "\n"
+  str << url
 end
 
 def send_to_dev_underground(msg)
