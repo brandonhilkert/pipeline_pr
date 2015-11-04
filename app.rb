63fafe9609fd9cfb1596124f7cc9c33f82be3428
Brandon Hilkert <brandonhilkert@gmail.com>
2015-11-04 16:25:33 -0500

    Add slack notifications

--- app.rb
+++ app.rb
@@ -12,10 +12,12 @@ post '/' do
     # Only trigger message when a new PR is opened
     if payload['action'] == 'opened'
       msg = message_for_new_pr(payload['pull_request'])
-      send_to_dev_underground(msg)
+      send_to_dev_underground_in_hipchat(msg)
+      send_to_engineering_in_slack(msg)
     elsif payload['action'] == 'closed' && payload['pull_request']['merged']
       msg = message_for_pr_merge(payload['pull_request'])
-      send_to_operations_talk(msg)
+      send_to_operations_talk_in_hipchat(msg)
+      send_to_operations_in_slack(msg)
     end
   end
 end
@@ -28,12 +30,20 @@ def message_for_pr_merge(pr)
   "<strong>MERGED</strong>: #{pr['head']['repo']['name']}/#{pr['number']} - <a href='#{pr['html_url']}' >#{pr['title']}</a> (#{pr['user']['login']})"
 end
 
-def send_to_dev_underground(msg)
+def send_to_dev_underground_in_hipchat(msg)
   hipchat_client['Engineering'].send('Github', msg)
 end
 
-def send_to_operations_talk(msg)
+def send_to_operations_talk_in_hipchat(msg)
   hipchat_client['Operations talk'].send('Github', msg)
+end
+
+def send_to_engineering_in_slack(msg)
+  RestClient.post ENV["SLACK_WEBHOOK_URL"], { text: msg }.to_json, content_type: :json, accept: :json
+end
+
+def send_to_operations_in_slack(msg)
+  RestClient.post ENV["SLACK_WEBHOOK_URL"], { text: msg, channel: "#operations" }.to_json, content_type: :json, accept: :json
 end
 
 def hipchat_client
