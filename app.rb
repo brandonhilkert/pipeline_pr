8be2b40baedb3b1000602091f0669c162cba3cfb
Brandon Hilkert <brandonhilkert@gmail.com>
2015-11-05 07:07:38 -0500

    Try slack formatting

--- app.rb
+++ app.rb
@@ -11,42 +11,47 @@ post '/' do
 
     # Only trigger message when a new PR is opened
     if payload['action'] == 'opened'
-      msg = message_for_new_pr(payload['pull_request'])
-      send_to_dev_underground_in_hipchat(msg)
-      send_to_engineering_in_slack(msg)
+      send_to_dev_underground_in_hipchat(payload['pull_request'])
+      send_to_engineering_in_slack(payload['pull_request'])
     elsif payload['action'] == 'closed' && payload['pull_request']['merged']
-      msg = message_for_pr_merge(payload['pull_request'])
-      send_to_operations_talk_in_hipchat(msg)
-      send_to_operations_in_slack(msg)
+      send_to_operations_talk_in_hipchat(payload['pull_request'])
+      send_to_operations_in_slack(payload['pull_request'])
     end
   end
 end
 
-def message_for_new_pr(pr)
-  "#{pr['head']['repo']['name']}/#{pr['number']} - <a href='#{pr['html_url']}' >#{pr['title']}</a> (#{pr['user']['login']})"
-end
-
-def message_for_pr_merge(pr)
-  "<strong>MERGED</strong>: #{pr['head']['repo']['name']}/#{pr['number']} - <a href='#{pr['html_url']}' >#{pr['title']}</a> (#{pr['user']['login']})"
-end
-
-def send_to_dev_underground_in_hipchat(msg)
+def send_to_dev_underground_in_hipchat(pr)
+  msg = "#{pr['head']['repo']['name']}/#{pr['number']} - <a href='#{pr['html_url']}' >#{pr['title']}</a> (#{pr['user']['login']})"
   hipchat_client['Engineering'].send('Github', msg)
 end
 
-def send_to_operations_talk_in_hipchat(msg)
+def send_to_operations_talk_in_hipchat(pr)
+  msg = "<strong>MERGED</strong>: #{pr['head']['repo']['name']}/#{pr['number']} - <a href='#{pr['html_url']}' >#{pr['title']}</a> (#{pr['user']['login']})"
   hipchat_client['Operations talk'].send('Github', msg)
 end
 
-def send_to_engineering_in_slack(msg)
-  RestClient.post ENV["SLACK_WEBHOOK_URL"], { text: msg }.to_json, content_type: :json, accept: :json
+def send_to_engineering_in_slack(pr)
+  text = "*#{pr['head']['repo']['name']}/#{pr['number']}* - <#{pr['html_url']}|#{pr['title']}> (#{pr['user']['login']})"
+  attachment = {
+    text: text,
+    mrkdwn_in: ["text"],
+  }
+  RestClient.post ENV["SLACK_WEBHOOK_URL"], { attachments: [attachment] }.to_json, content_type: :json, accept: :json
 end
 
-def send_to_operations_in_slack(msg)
-  RestClient.post ENV["SLACK_WEBHOOK_URL"], { text: msg, channel: "#operations" }.to_json, content_type: :json, accept: :json
+def send_to_operations_in_slack(pr)
+  text = "*MERGED*: #{pr['head']['repo']['name']}/#{pr['number']} - <#{pr['html_url']}|#{pr['title']}> (#{pr['user']['login']})"
+  attachment = {
+    text: text,
+    mrkdwn_in: ["text"],
+  }
+  RestClient.post ENV["SLACK_WEBHOOK_URL"], { attachments: [attachment], channel: "#operations" }.to_json, content_type: :json, accept: :json
+end
+
+def slack_attachment(text)
+
 end
 
 def hipchat_client
   HipChat::Client.new(ENV['HIPCHAT_API'])
 end
-
